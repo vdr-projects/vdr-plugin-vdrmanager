24528f410712a25d17e586dd31a0615e2ec86464
lado <herrlado@gmail.com>
2011-10-06 02:43:23 +0200

    recording adapter, initial version

---
+++ vdrmanager/src/de/bjusystems/vdrmanager/gui/RecordingAdapter.java
@@ -0,0 +1,21 @@
+package de.bjusystems.vdrmanager.gui;
+
+import android.content.Context;
+import de.bjusystems.vdrmanager.R;
+import de.bjusystems.vdrmanager.data.Event;
+import de.bjusystems.vdrmanager.data.EventFormatter;
+
+class RecordingAdapter extends EventAdapter
+{
+
+	
+	public RecordingAdapter(final Context context) {
+		super(context, R.layout.epg_event_item);
+		hideChannelName = false;
+	}
+	
+	@Override
+	protected EventFormatter getEventFormatter(Event event) {
+			return new EventFormatter(event, true);
+	}
+}
