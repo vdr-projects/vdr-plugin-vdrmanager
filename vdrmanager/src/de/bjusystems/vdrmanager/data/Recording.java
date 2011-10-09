43b2a56848507d3f4e1e5ff8c5fe2e6f31ff7a13
lado <herrlado@gmail.com>
2011-10-10 01:16:27 +0200

    filesize added

--- vdrmanager/src/de/bjusystems/vdrmanager/data/Recording.java
+++ vdrmanager/src/de/bjusystems/vdrmanager/data/Recording.java
@@ -9,15 +9,26 @@ public class Recording extends BaseEvent{
 		super(null);
 		final String[] words = line.split(":");
 		start = new Date(Long.parseLong(words[0])*1000);
-		stop = new Date(start.getTime() - 1000 * 1 * 60);
-		channelName = words[2];
-		title = words[3];
-		shortText = words[4];
-		description = words[5];
-		fileName = words[6];
+		stop = new Date(Long.parseLong(words[1]) * 1000);
+		fileSize = Integer.valueOf(words[2]);
+		channelName = words[3];
+		title = words[4];
+		shortText = words[5];
+		description = words[6];
+		fileName = words[7];
 	}
 	
 	private String fileName;
+
+	private int fileSize;
+
+	public int getFileSize() {
+		return fileSize;
+	}
+
+	public void setFileSize(int fileSize) {
+		this.fileSize = fileSize;
+	}
 
 	public String getFileName() {
 		return fileName;
