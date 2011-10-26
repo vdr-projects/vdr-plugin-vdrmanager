a5e619465700e77bfbdb44a997edeefacfee99fc
lado <herrlado@gmail.com>
2011-10-26 13:28:43 +0200

    constane

---
+++ vdrmanager/src/de/bjusystems/vdrmanager/app/C.java
@@ -0,0 +1,15 @@
+package de.bjusystems.vdrmanager.app;
+
+/**
+ * 
+ * Some constants
+ * @author lado
+ * 
+ *
+ */
+public interface C {
+
+	public static final String DATA_SEPARATOR = ":";
+	
+	public static final long ONE_MINUTE_IN_MILLIS = 60 * 1000;
+}
