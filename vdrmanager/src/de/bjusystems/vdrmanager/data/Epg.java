0e0db3a259809c95df433ab315a695fe06c14b45
lado <herrlado@gmail.com>
2011-10-26 14:53:58 +0200

    mapSpecialChars

--- vdrmanager/src/de/bjusystems/vdrmanager/data/Epg.java
+++ vdrmanager/src/de/bjusystems/vdrmanager/data/Epg.java
@@ -2,6 +2,12 @@ package de.bjusystems.vdrmanager.data;
 
 import java.util.Date;
 
+
+import de.bjusystems.vdrmanager.StringUtils;
+import de.bjusystems.vdrmanager.app.C;
+
+import static de.bjusystems.vdrmanager.gui.Utils.mapSpecialChars;
+
 /**
  * Class for EPG events
  * @author bju
@@ -11,14 +17,14 @@ public class Epg extends BaseEvent {
 	private Timer timer;
 
 	public Epg(final String line) {
-		final String[] words = line.split(":");
+		final String[] words = StringUtils.splitPreserveAllTokens(line, C.DATA_SEPARATOR);
 		channelNumber = words[0].substring(1);
 		channelName = words[1];
 		start = new Date(Long.parseLong(words[2])*1000);
 		stop = new Date(Long.parseLong(words[3])*1000);
-		title = words[4];
-		description = words.length > 5 ? words[5] : "";
-		shortText = words.length > 6 ? words[6] : "";
+		title = mapSpecialChars(words[4]);
+		description = words.length > 5 ? mapSpecialChars(words[5]): "";
+		shortText = words.length > 6 ? mapSpecialChars(words[6]) : "";
 	}
 	
 
