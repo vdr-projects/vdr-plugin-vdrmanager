48c417b2bdc155cce4404c4d45ea1434c4ac9ea3
lado <herrlado@gmail.com>
2012-01-16 13:12:35 +0100

    initial files for DES

---
+++ vdrmanager/jni/de_bjusystems_vdrmanager_utils_crypt_NativeDES.c
@@ -0,0 +1,24 @@
+#include "de_bjusystems_vdrmanager_utils_crypt_NativeDES.h"
+
+ /*
+  * Class:     de_bjusystems_vdrmanager_utils_crypt_NativeDES
+  * Method:    encrypt
+  * Signature: (Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;
+  */
+ JNIEXPORT jstring JNICALL Java_de_bjusystems_vdrmanager_utils_crypt_NativeDES_encrypt(
+ 		JNIEnv *env, jobject c, jstring jstr, jstring jkey) {
+
+ 	return jstr;
+ }
+
+ /*
+  * Class:     de_bjusystems_vdrmanager_utils_crypt_NativeDES
+  * Method:    decrypt
+  * Signature: (Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;
+  */
+ JNIEXPORT jstring JNICALL Java_de_bjusystems_vdrmanager_utils_crypt_NativeDES_decrypt(
+ 		JNIEnv *env, jobject c, jstring jstr, jstring key) {
+
+	 return jstr;
+ }
+
