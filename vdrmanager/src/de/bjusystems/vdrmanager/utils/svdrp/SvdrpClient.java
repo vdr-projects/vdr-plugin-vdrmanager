d88f4a8971b41d5a823e41acd5ef58086cd0be06
lado <herrlado@gmail.com>
2012-01-16 13:15:04 +0100

    -imports

--- vdrmanager/src/de/bjusystems/vdrmanager/utils/svdrp/SvdrpClient.java
+++ vdrmanager/src/de/bjusystems/vdrmanager/utils/svdrp/SvdrpClient.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.zip.InflaterInputStream;
 
 import android.util.Log;
 import de.bjusystems.vdrmanager.app.C;
