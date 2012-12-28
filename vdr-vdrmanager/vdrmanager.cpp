46df924635eba4e742cb151319b8010fe87b9694
lado <herrlado@gmail.com>
2012-12-28 14:15:46 +0100

    version info to 0.0.9

--- vdr-vdrmanager/vdrmanager.cpp
+++ vdr-vdrmanager/vdrmanager.cpp
@@ -15,7 +15,7 @@
 
 #define ANDROVDR_PORT		6420
 
-const char *VERSION = "0.8";
+static const char *VERSION = "0.9";
 static const char *DESCRIPTION = "VDR-Manager support plugin";
 
 class cPluginAndroVdr: public cPlugin {
