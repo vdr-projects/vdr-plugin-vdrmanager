20d03e5a34d6cbd71b76c09a9203fcd1fb342a66
bju <bju@maxi.fritz.box>
2017-04-22 15:49:17 +0200

    Revert "toggletimer setenv"
    This reverts commit 5a379415c2d47533d5548c97d44d024234b45542.

--- vdr-vdrmanager/helpers.cpp
+++ vdr-vdrmanager/helpers.cpp
@@ -550,7 +550,6 @@ string cHelpers::SetTimerIntern(char op, string param) {
 		}
 
 		toggleTimer->OnOff();
-                toggleTimer->Append(clearenv())
 		theTimers->SetModified();
 		dsyslog("[vdrmanager] timer %s toggled %s", *toggleTimer->ToDescr(),
 				toggleTimer->HasFlags(tfActive) ? "on" : "off");
